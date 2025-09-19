75565f06c9f984921586065ca500c829b547fcf8
Eugenio Liguori <e.liguori@gexy.it>
2025-09-19 02:49:36 +0200

    Split JSchema from JObject

---
+++ validator/JSchema.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLab.GeJSON.validator
+{
+    public class JSchema : JObject
+    {
+        public JSchema() { }
+        public JSchema(string rawJson) : base(rawJson)
+        {
+        }
+
+       
+    }
+}
---
+++ validator/ValidatorException.cs
@@ -0,0 +1,41 @@
+﻿using OpenLab.GeJSON.parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLab.GeJSON.validator
+{
+    public class ValidatorException(string Message) : Exception(Message)
+    {
+      
+
+        public Token parserToken { get; set; }
+        public dynamic jsonEntity { get; set; }
+
+        public override string ToString()
+        {
+            string o = "";
+
+            if (jsonEntity != null)
+            {
+                o += jsonEntity.ToString(false);
+                o += Environment.NewLine;
+                o += Environment.NewLine;
+            }
+
+            if (parserToken != null)
+            {
+                o += parserToken.ToString();
+                o += Environment.NewLine;
+                o += Environment.NewLine;
+            }
+            
+            o += base.ToString();
+
+            return o;
+        }
+
+    }
+}
