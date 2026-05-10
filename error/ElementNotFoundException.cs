7b605ede0cf3cd97a599f8ade530307556ffb399
Eugenio Liguori <e.liguori@gexy.it>
2026-05-10 17:15:53 +0200

    Add exception

---
+++ error/ElementNotFoundException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenLab.GeJSON.error
+{
+    public class ElementNotFoundException : Exception
+    {
+        public JObject element;
+        public JArray array;
+        public JObject Object;
+        public int arrayIndex;
+        public ElementNotFoundException() : base(){ }
+        public ElementNotFoundException(string Message) : base(Message) { }
+        public ElementNotFoundException(JObject element) : base() {
+            this.element = element;
+        }
+        public ElementNotFoundException(JObject element, string Message) : base(Message)
+        {
+            this.element = element;
+        }
+
+        public ElementNotFoundException(JArray array, int index) : base()
+        {
+            this.array = array;
+            this.arrayIndex = index;
+        }
+        public ElementNotFoundException(JArray array, int index, string Message) : base(Message)
+        {
+            this.array = array;
+            this.arrayIndex = index;
+        }
+    }
+}
